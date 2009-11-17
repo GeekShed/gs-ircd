f23acfc6417a95172897cf3d87ec06918344f493
Corey Maertz <corey@geekshed.net>
2009-11-17 03:01:00 +0000

    changing 420 back to unreal default to see what happens

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -417,7 +417,11 @@ int  inetport(aClient *cptr, char *name, int port, int options)
 		server.SIN_ADDR.S_ADDR = inet_addr(ipname);
 #else
 		if (options & LISTENER_SCTP) {
-			server.SIN_ADDR.S_ADDR = INADDR_ANY;
+		/*	server.SIN_ADDR.S_ADDR = INADDR_ANY;
+		eh.. maybe my way will work?
+		*/
+			server.SIN_ADDR.S_ADDR = inet_addr(ipname);
+
 		} else {
 			inet_pton(AFINET, ipname, server.SIN_ADDR.S_ADDR);
 		}
@@ -2583,6 +2587,7 @@ static struct SOCKADDR *connect_inet(ConfigItem_link *aconf, aClient *cptr, int 
 		server.SIN_ADDR.S_ADDR = inet_addr(aconf->bindip);	
 #else
 	if (aconf->options & CONNECT_SCTP) {
+		
 		server.SIN_ADDR.S_ADDR = INADDR_ANY;
 	} else {
 		inet_pton(AF_INET6, aconf->bindip, server.SIN_ADDR.S_ADDR);
