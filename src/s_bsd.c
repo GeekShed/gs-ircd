16eed4729b0c58f4a645082800c125ef53a55d8d
Dylan Cochran <helio@geekshed.net>
2009-11-19 07:18:02 +0000

    * Revert another test change.

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -414,11 +414,7 @@ int  inetport(aClient *cptr, char *name, int port, int options)
 		/* per-port bindings, fixes /stats l */
 		
 #ifndef INET6
-		if (options & LISTENER_SCTP) {
-			server.SIN_ADDR.S_ADDR = INADDR_ANY;
-		} else {
 			server.SIN_ADDR.S_ADDR = inet_addr(ipname);
-		}
 #else
 			inet_pton(AFINET, ipname, server.SIN_ADDR.S_ADDR);
 #endif
@@ -2580,11 +2576,7 @@ static struct SOCKADDR *connect_inet(ConfigItem_link *aconf, aClient *cptr, int 
 	if (aconf->bindip && strcmp("*", aconf->bindip))
 	{
 #ifndef INET6
-	if (aconf->options & CONNECT_SCTP) {
-		server.SIN_ADDR.S_ADDR = INADDR_ANY;
-	} else {
 		server.SIN_ADDR.S_ADDR = inet_addr(aconf->bindip);	
-	}
 #else
 		inet_pton(AF_INET6, aconf->bindip, server.SIN_ADDR.S_ADDR);
 #endif
