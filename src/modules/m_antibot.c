ca5425b245fa258dbaa944520cc9da9ba0b9ded6
Phil Lavin <phil@geekshed.net>
2010-07-28 20:53:32 +0000

    Further time tweak

--- src/modules/m_antibot.c
+++ src/modules/m_antibot.c
@@ -86,7 +86,7 @@ DLLFUNC int m_lconnect(aClient *cptr)
 		sendto_one(cptr, ":%s NOTICE %s :*** Please wait while we scan your connection for open proxies...", me.name, cptr->name);
 		sendto_snomask_global(SNO_BOPM, "BOPM unknown unknown %s %s", Inet_ia2p(&cptr->ip), Inet_ia2p(&cptr->ip));
 		now = TStime();
-		cptr->since = now + 20;
+		cptr->since = now + 15;
 	return 0;
 }
 
